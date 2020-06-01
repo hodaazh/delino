eaaa585671ba45e4d4e7e44641237db10be67a2b
hodaazh97 <hoda.ahmadzadeh97@gmail.com>
2020-06-02 01:05:53 +0430

    finish 50 percent

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import MainScreen from "./screen/MainScreen/MainScreen";
+import Cart from "./components/Cart/Cart";
+import SlideShow from "./components/SlideShow/SlideShow";
+import Message from "./components/Message/Message";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MainScreen />
+      {/* <Cart /> */}
+      {/* <SlideShow /> */}
+      {/* <Message /> */}
     </div>
   );
 }
